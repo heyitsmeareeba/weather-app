da54c710d48889034031e06b0eef3f8404cc918f
AREEBA <areebalakhani131@gmail.com>
2024-08-21 10:24:07 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+            var navLinks = document.getElementById("navLinks");
+            function showMenu() {
+                navLinks.style.right ="0";
+            }
+            function hideMenu() {
+                navLinks.style.right ="-200px";
+            }
+            let O= menu.getElementById(navbar);
+            const navbar{
+                "Home"=openpage();
+                "Our story"=openpage(Ourstoryh);
+                "Patner with us"=openpage(patner with us)
+                "Career"= openpage(career);
+            }
+            function openpage(){
+                
+                
+            }
+        const ;
+        document.getElementById("Ourstoryh").style.display = "block";
+
+    <div onclick="document.getElementById('Ourstoryh').display="Ourstoryh"></onclick>
